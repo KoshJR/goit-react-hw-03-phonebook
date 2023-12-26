976006fdac138b463b57182093ba23a76f472ff0
KoshJR <goldenarm31.03@gmail.com>
2023-12-26 15:23:08 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,22 @@ import { FormAddContacts } from 'components/Form/Form';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const stringifyContacts = localStorage.getItem('contacts');
+    const contacts = JSON.parse(stringifyContacts) ?? [];
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      const stringifyContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem('contacts', stringifyContacts);
+    }
+  }
 
   handleAddContact = (contact, formReset) => {
     const newContact = {
